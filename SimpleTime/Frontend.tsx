2865f0b4f46c3180934b8dcc56a349967b3c435c
Kove W. Ochre-Salter <kove.w.o.salter@gmail.com>
2019-07-30 16:16:20 +1000

    Improve stylesheets
    * Improve/simplify stylesheets.
    * Standardize colors.

--- SimpleTime/Frontend.tsx
+++ SimpleTime/Frontend.tsx
@@ -6,27 +6,23 @@ import {
     Image
 } from "react-native";
 import Backend from "./Backend";
-import Styles from "../assets/style/Style";
+import {styleSheet, Color} from "../assets/style/Style";
 
 export default class Frontend extends React.Component {
     public render(): JSX.Element {
         let sunAngle: number = this.backend.sunAngle();
-
-        if(sunAngle > 90 && sunAngle < 270) {
-            this.styles.enableDarkTheme();
-        } else {
-            this.styles.disableDarkTheme();
-        }
+        let usingDarkTheme: boolean = sunAngle > 90 && sunAngle < 270;
+        let styles: any = styleSheet(usingDarkTheme);
 
         return (
-            <View style={this.styles.styles().container}>
-                <Text style={[this.styles.styles().text, this.styles.styles().time]}>
+            <View style={styles.container}>
+                <Text style={[styles.text, styles.time]}>
                     {this.backend.simpleTime()}
                 </Text>
                 <Image source={require("../assets/img/day.png")} style={{ transform: [{ rotateZ: `${this.backend.sunAngle()}deg` }] }} />
-                <TouchableHighlight onPress={this.backend.saySimpleTime} underlayColor={this.styles.usingDarkTheme() ? "#212121" : "#ffffff"} activeOpacity={0.5}>
-                    <View style={this.styles.styles().button}>
-                        <Text style={this.styles.styles().text}>Say SimpleTime</Text>
+                <TouchableHighlight onPress={this.backend.saySimpleTime} underlayColor={usingDarkTheme ? Color.BackgroundDark : Color.BackgroundLight} activeOpacity={0.5}>
+                    <View style={styles.button}>
+                        <Text style={[styles.text, styles.buttonText]}>Say SimpleTime</Text>
                     </View>
                 </TouchableHighlight>
             </View>
@@ -46,5 +42,4 @@ export default class Frontend extends React.Component {
 
     private interval: number;
     private backend: Backend = new Backend();
-    private styles: Styles = new Styles();
 }
--- assets/style/Style.tsx
+++ assets/style/Style.tsx
@@ -1,71 +1,67 @@
 import {StyleSheet} from "react-native";
 
-export default class Styles {
-    public constructor() {
-        this.disableDarkTheme();
-    }
+export function styleSheet(useDarkTheme: boolean) {
+    return StyleSheet.create({
+        container: {
+            flex: 1,
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            color: useDarkTheme ? Color.ForegroundDark : Color.ForegroundLight,
+            backgroundColor: useDarkTheme ? Color.BackgroundDark : Color.BackgroundLight
+        },
+        text: {
+            fontFamily: "monospace"
+        },
+        time: {
+            color: useDarkTheme ? Color.ForegroundDark : Color.ForegroundLight,
+            fontSize: 40,
+            flexWrap: "wrap",
+            textAlign: "center",
+            marginLeft: Margin.Left,
+            marginRight: Margin.Right,
+            marginBottom: Margin.Bottom
+        },
+        button: {
+            backgroundColor: Color.Primary,
+            paddingTop: Padding.Top,
+            paddingBottom: Padding.Bottom,
+            paddingRight: Padding.Right,
+            paddingLeft: Padding.Left,
+            borderRadius: 5,
+            elevation: 5,
+            marginTop: Margin.Top,
+            fontFamily: "monospace"
+        },
+        buttonText: {
+            color: Color.ForegroundDark
+        }
+    });
+}
 
-    public usingDarkTheme = (): boolean => {
-        return this.useDarkTheme;
-    }
+export enum Color {
+    Primary = "#6200ee",
+    PrimaryVariant = "#3700b3",
+    Secondary = "#03dac6",
+    SecondaryVariant = "#018786",
 
-    public enableDarkTheme = (): boolean => {
-        return (this.useDarkTheme = true);
-    }
+    BackgroundDark = "#212121",
+    BackgroundLight = "#ffffff",
 
-    public disableDarkTheme = (): boolean => {
-        return (this.useDarkTheme = false);
-    }
+    ForegroundDark = "#ffffff",
+    ForegroundLight = "#000000"
+}
 
-    public toggleDarkTheme = (): boolean => {
-        return (this.useDarkTheme = !this.useDarkTheme);
-    }
+enum Padding {
+    Left = 20,
+    Right = 20,
+    Bottom = 20,
+    Top = 20
+}
 
-    public styles = (): any => {
-        return this.createStyles(this.usingDarkTheme())
-    }
-
-    private createStyles(_useDarkTheme: boolean): any {
-        return StyleSheet.create({
-            container: {
-                flex: 1,
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                color: _useDarkTheme ? this.colors.DarkForeground : this.colors.LightForeground,
-                backgroundColor: _useDarkTheme ? this.colors.DarkBackground : this.colors.LightBackground
-            },
-            text: {
-                color: _useDarkTheme ? this.colors.DarkForeground : this.colors.LightForeground,
-                fontFamily: "monospace"
-            },
-            time: {
-                fontSize: 40,
-                flexWrap: "wrap",
-                textAlign: "center",
-                marginLeft: 20,
-                marginRight: 20,
-                marginBottom: 20
-            },
-            button: {
-                backgroundColor: _useDarkTheme ? this.colors.DarkButton : this.colors.LightButton,
-                padding: 20,
-                borderRadius: 5,
-                elevation: 5,
-                marginTop: 20,
-                fontFamily: "monospace"
-            }
-        });
-    }
-
-    private useDarkTheme: boolean = false;
-    private colors: any = {
-        LightBackground: "#ffffff",
-        DarkBackground: "#212121",
-        LightForeground: "#000000",
-        DarkForeground: "#ffffff",
-
-        DarkButton: "#303030",
-        LightButton: "#d3d3d3"
-    };
+enum Margin {
+    Left = 20,
+    Right = 20,
+    Bottom = 20,
+    Top = 20
 }
